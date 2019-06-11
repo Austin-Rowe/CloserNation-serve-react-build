8b0ac31ddf67e485b9b9af91d77bf09c766fb507
Austin Rowe <amrowe2018@gmail.com>
2019-06-11 00:49:14 -0500

    Updating for use with best closer show.

---
+++ build/precache-manifest.b7d19aa3235b7c7f63345e068a070833.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90fa3509303f078b0f2694129c3b56c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b33b54723c22bdcffa18",
+    "url": "/static/css/main.87491995.chunk.css"
+  },
+  {
+    "revision": "4b4410b9a6168c868f77",
+    "url": "/static/js/2.ca4ab5d1.chunk.js"
+  },
+  {
+    "revision": "b33b54723c22bdcffa18",
+    "url": "/static/js/main.cecfc89d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
