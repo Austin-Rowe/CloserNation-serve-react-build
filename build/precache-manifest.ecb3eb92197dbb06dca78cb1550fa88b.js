6abad76dfced72ce97bc9831b3f2256a3ffa7d0c
Austin Rowe <amrowe2018@gmail.com>
2019-06-21 21:42:01 -0500

    New build with reset password component added.

---
+++ build/precache-manifest.ecb3eb92197dbb06dca78cb1550fa88b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e099e80175c3ef61343d8abdde4dec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92c51811024e937512e8",
+    "url": "/static/css/main.e9a70aa6.chunk.css"
+  },
+  {
+    "revision": "e1f2da7201d8a779b621",
+    "url": "/static/js/2.925f9dcf.chunk.js"
+  },
+  {
+    "revision": "92c51811024e937512e8",
+    "url": "/static/js/main.ff3c1497.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.338e80da124fc6199694ac7e1482e574.js"
+  "/precache-manifest.ecb3eb92197dbb06dca78cb1550fa88b.js"
 );
 
 self.addEventListener('message', (event) => {
